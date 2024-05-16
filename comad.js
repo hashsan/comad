b4ccfb2a2e57273137d3bfce743d40f556e106c7
はっしゅさん <149641384+hashsan@users.noreply.github.com>
2024-05-16 09:37:36 +0900

    Create comad.js

---
+++ comad.js
@@ -0,0 +1,54 @@
+//module comad
+
+import "https://cdnjs.cloudflare.com/ajax/libs/marked/12.0.2/marked.min.js";
+
+function comad(text,updateCaller){
+  //updatecallerは呼び出すだけで何もしない。
+  var o={}
+  o.parse = marked.parse
+
+  o.view = document.createElement('div')
+  {
+    o.view.classList.add('view')    
+  }
+  o.edit = document.createElement('div')
+  {
+    o.edit.classList.add('edit')
+    o.edit.contentEditable = 'plaintext-only'
+    o.edit.style.outline ='none'
+    o.edit.textContent = text||''
+  }
+
+  o.comad = document.createElement('div')
+  {
+    o.comad.classList.add('comad')
+    o.comad.append(o.view)
+    o.comad.append(o.edit)
+  }
+
+  o.showView =()=>{
+    o.edit.style.display ='none'
+    o.view.style.display ='block'
+    const text = o.edit.textContent
+    o.view.innerHTML = o.parse(text)
+    if(updateCaller){
+      updateCaller()
+    }
+  }
+  
+  o.showEdit =()=>{
+    o.edit.style.display ='block'
+    o.view.style.display ='none'
+    
+    o.edit.focus()
+  }
+
+  ////
+  o.view.onclick = o.showEdit
+  o.edit.onblur = o.showView  
+  o.showView();
+  //showView,showEdit,el,parse,setText
+  return o;
+}
+
+window.comad = comad;
